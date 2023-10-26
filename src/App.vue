4c661a7b043f19cd628f7002071f0220a4574e77
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-10-26 20:44:43 +0900

    :lipstick: Timer 영역 레이아웃 구성 및 스타일 적용
    Timer 영역 레이아웃 구성 및 스타일 적용

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
-<template></template>
+<template>
+  <section class="timer">
+    <div>
+      <h2>timerText</h2>
+      <h1>remainTime</h1>
+    </div>
+  </section>
+</template>
 
 <script setup lang="ts"></script>
 
-<style></style>
+<style>
+.timer {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 2rem;
+}
+</style>
