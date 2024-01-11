8d0ed4d2120900f0cfb4981704a165b40b71caaf
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2024-01-11 18:28:46 +0900

    :wrench: 메타 데이터 추가 및 manifest 옵션 추가
    메타 데이터 추가 및 manifest 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, './dist'),
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon-180x180.png',
+        'maskable-icon-512x512.png',
+      ],
+      manifest: {
+        name: 'Pomodoro Timer',
+        short_name: 'Pomodoro',
+        description: 'Pomodoro Timer with Vue',
+        background_color: 'transparent',
+        theme_color: '#34495e',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
